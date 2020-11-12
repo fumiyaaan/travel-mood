564820525811f31be2b5dfac4632aa26ef6b0753
fumiyaaan <indestructible.peace@gmail.com>
2020-11-12 22:16:08 +0900

    投稿機能の大枠作成

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,4 +1,21 @@
 class SpotsController < ApplicationController
   def index
+    @spots = Spot.all
   end
+
+  def new
+    @spot = Spot.new
+  end
+
+  def create
+    
+    Spot.create(spot_params)
+  end
+
+  private
+
+  def spot_params
+    params.require(:spot).permit(:image, :title, :description).merge(user_id: current_user.id)
+  end
+  
 end
