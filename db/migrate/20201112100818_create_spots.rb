8688bcc7e236f645a622101210d2952788494e37
fumiyaaan <indestructible.peace@gmail.com>
2020-11-12 19:18:54 +0900

    spotテーブル作成

--- db/migrate/20201112100818_create_spots.rb
+++ db/migrate/20201112100818_create_spots.rb
@@ -1,7 +1,9 @@
 class CreateSpots < ActiveRecord::Migration[6.0]
   def change
     create_table :spots do |t|
-
+      t.string     :title,       null: false
+      t.text       :description, null: false
+      t.references :user,        foreign_key: true
       t.timestamps
     end
   end
