41888082018a9b8ecaa17e1b34c2d63878c5d940
fumiyaaan <indestructible.peace@gmail.com>
2020-11-14 13:28:52 +0900

    spotモデル単体テストコード

--- spec/models/spot_spec.rb
+++ spec/models/spot_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Spot, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @spot = FactoryBot.build(:spot)
+  end
+
+  describe 'スポットの投稿' do
+    context "スポットの投稿がうまくいくとき" do
+      it "image, title, descriptionがあれば投稿できる" do
+        expect(@spot).to be_valid
+      end
+    end
+
+    context "スポットの投稿がうまくいかないとき" do
+      it "imageがなければ投稿できない" do
+        @spot.image = nil
+        @spot.valid?
+        expect(@spot.errors.full_messages).to include("Image can't be blank")
+      end
+
+      it "titleがなければ投稿できない" do
+        @spot.title = ""
+        @spot.valid?
+        expect(@spot.errors.full_messages).to include("Title can't be blank")
+      end
+
+      it "descriptionがなければ投稿できない" do
+        @spot.description = nil
+        @spot.valid?
+        expect(@spot.errors.full_messages).to include("Description can't be blank")
+      end
+    end
+  end
 end
