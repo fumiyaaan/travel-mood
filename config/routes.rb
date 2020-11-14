0e90f506887cb316a6452e299f9e2e58dca8fc66
fumiyaaan <indestructible.peace@gmail.com>
2020-11-14 17:37:56 +0900

    showアクション設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  resources :spots, only: [:index, :new, :create]
+  resources :spots, only: [:index, :new, :create, :show]
   get 'homes', to: 'homes#about'
 end
