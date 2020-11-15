670394a44d1011039346c6b414f2722c3f7dedf6
fumiyaaan <indestructible.peace@gmail.com>
2020-11-15 15:46:31 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  resources :spots, only: [:index, :new, :create, :show]
+  resources :spots, only: [:index, :new, :create, :show, :edit, :update]
   get 'homes', to: 'homes#about'
 end
