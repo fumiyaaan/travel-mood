b90f52f4444e68cb744a1b722d13dc2779877576
fumiyaaan <indestructible.peace@gmail.com>
2020-11-16 11:20:39 +0900

    バリデーション設定

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
+  validates :text, presence: true
+
+  UNKNOWN_REGEX = /\A[1-5]\z/
+  validates :unknown, presence: true
+  validates_format_of :unknown, with: UNKNOWN_REGEX, message: 'Input one number within 1 to 5'
+
   belongs_to :user
   belongs_to :spot
-end
+endails
