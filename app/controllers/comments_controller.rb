8c854c9be6adcfa9c7f2ce7b1785f308610ee60c
fumiyaaan <indestructible.peace@gmail.com>
2020-11-16 11:31:45 +0900

    createアクション設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,17 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to spot_path(@spot.id)
+    else
+      render template: 'spots/show'
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text, :unknown).merge(user_id: current_user.id, spot_id: params[:spot_id])
+  end
+  
 end
