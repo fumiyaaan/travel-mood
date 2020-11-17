afaf6e4421a0a6aa698472c736f5f82a1c2818d8
fumiyaaan <indestructible.peace@gmail.com>
2020-11-17 21:11:43 +0900

    ビュー途中経過

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,21 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    if @user.update(user_params)
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile).merge(id: current_user.id)
+  end
+
 end
