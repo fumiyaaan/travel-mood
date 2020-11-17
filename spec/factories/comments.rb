5fc3220cf5c798e64322f5930cb593920bba05ea
fumiyaaan <indestructible.peace@gmail.com>
2020-11-17 15:53:56 +0900

    commentモデル単体テストコード

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :comment do
-    
+    text { '綺麗な景色ですね' }
+    unknown { 5 }
+    association :spot
+    association :user
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @comment = FactoryBot.build(:comment)
+  end
+
+  describe 'コメントの投稿' do
+    context 'コメントの投稿がうまくいくとき' do
+      it 'text, unknownがあれば投稿できる' do
+        expect(@comment).to be_valid
+      end
+    end
+
+    context 'コメントの投稿がうまくいかないとき' do
+      it 'textがなければ投稿できない' do
+        @comment.text = ""
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Text can't be blank")
+      end
+
+      it 'unknownがなければ投稿できない' do
+        @comment.unknown = ""
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Unknown can't be blank")
+      end
+
+      it 'unknownが1~5の半角数字で入力されていなければ投稿できない' do
+        @comment.unknown = 6
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Unknown Input one number within 1 to 5")
+      end
+      
+    end
+  end
 end
