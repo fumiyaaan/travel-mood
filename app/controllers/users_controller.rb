1b8533ca5d6a32a942edb3644bfef01d9b1a3c3f
fumiyaaan <indestructible.peace@gmail.com>
2020-11-18 11:28:21 +0900

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,23 +5,4 @@ class UsersController < ApplicationController
     @profile = user.profile
     @spots = user.spots
   end
-
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def update
-    if @user.update(user_params)
-      redirect_to user_path(@user.id)
-    else
-      render :edit
-    end
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :profile).merge(id: current_user.id)
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :spots do
     resources :comments, only: :create
   end
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: :show
   get 'homes', to: 'homes#about'
 end
