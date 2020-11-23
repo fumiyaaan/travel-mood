459219923919178021eae8889b6f6280544b059c
fumiyaaan <indestructible.peace@gmail.com>
2020-11-23 11:25:21 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   resources :spots do
     resources :comments, only: :create
+    member do
+      get 'search'
+    end
   end
-  get 'spots', to: 'spots#search'
   resources :users, only: :show
   get 'homes', to: 'homes#about'
 end
