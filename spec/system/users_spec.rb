3a473cfc1b41c1715eda9be4a7717ffa4b2027c5
fumiyaaan <indestructible.peace@gmail.com>
2020-12-10 14:11:20 +0900

    結合テスト（ログイン）

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -66,3 +66,62 @@ RSpec.describe "ユーザー新規登録", type: :system do
     end
   end
 end
+
+RSpec.describe "ログイン", type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+  end
+
+  context 'ログインができるとき' do
+    it '保存されているユーザーの情報と合致すればログインができる' do
+      # トップページに移動する
+      visit root_path
+
+      # トップページにログインページに遷移するボタンがあることを確認する
+      expect(page).to have_content('ログイン')
+
+      # ログインページへ移動する
+      visit new_user_session_path
+
+      # 正しいユーザー情報を入力する
+      fill_in 'メールアドレス', with: @user.email
+      fill_in 'パスワード', with: @user.password
+
+      # ログインボタンを押す
+      find('input[name="commit"]').click
+
+      # トップページへ遷移することを確認する
+      expect(current_path).to eq root_path
+
+      # ログアウトボタンがあることを確認する
+      expect(page).to have_content('ログアウト')
+
+      # 新規登録ページへ遷移するボタンや、ログインページへ遷移するボタンが表示されていないことを確認する
+      expect(page).to have_no_content('新規登録')
+      expect(page).to have_no_content('ログイン')
+    end
+  end
+
+  context 'ログインができないとき' do
+    it '保存されているユーザーの情報と合致しないとログインができない' do
+      # トップページに移動する
+      visit root_path
+
+      # トップページにログインページに遷移するボタンがあることを確認する
+      expect(page).to have_content('ログイン')
+
+      # ログインページへ移動する
+      visit new_user_session_path
+
+      # 誤ったユーザー情報を入力する
+      fill_in 'メールアドレス', with: ""
+      fill_in 'パスワード', with: ""
+
+      # ログインボタンを押す
+      find('input[name="commit"]').click
+
+      # ログインページへ戻されることを確認する
+      expect(current_path).to eq new_user_session_path
+    end
+  end
+end
